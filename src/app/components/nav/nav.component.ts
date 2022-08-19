73c395b169510c53da31bd397f926b125f2f1dd8
Luiz Felipe Romani <luiz.tobaldini@cooperalfa.coop.br>
2022-08-19 17:06:00 -0300

    brincando com o CSS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['produtos'])
+    this.router.navigate(['produtos/create'])
   }
 
   logout() {
