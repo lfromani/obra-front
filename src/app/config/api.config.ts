424be1d1175693afacd324829f993bd69d4cee8e
Luiz Felipe Romani <luiz.tobaldini@cooperalfa.coop.br>
2022-09-29 14:26:19 -0300

    alterando para homo

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: 'https://obra-prod.herokuapp.com'
+    //baseUrl: 'https://obra-prod.herokuapp.com'
+    baseUrl: 'https://obra-homo.herokuapp.com'
     //baseUrl: 'http://localhost:8080'
 }
